879d370678041ba25843cef41c5f6401f0d1929d
Zinc <greenberg.zachary@utexas.edu>
2020-10-27 22:07:57 -0500

    started adding questions based on main menu

--- employee.js
+++ employee.js
@@ -29,35 +29,80 @@ function start() {
     ]).then((res) => {
         switch (res.action) {
             case "Add Employee":
-                employee();
-                break;
+              employee();
+              break;
             case "Add Role":
-                role();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-            case "Add Employee":
-                employee();
-                break;
-        }
+              role();
+              break;
+            case "Add Department":
+              department();
+              break;
+            case "View All Employees":
+              viewEmployees();
+              break;
+            case "View All Employees By Role":
+              viewByRole();
+              break;
+            case "View All Employees By Department":
+              viewByDepartment();
+              break;
+            case "View All Roles":
+              viewRoles();
+              break;
+            case "View All Departments":
+              viewDepartments();
+              break;
+            case "Update An Employee Role":
+              updateEmployee();
+              break;
+            case "Exit":
+              connection.end();
+              break;
+          }
     });
 };
+//View functions
+// View list of all employees
+function viewEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.department, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", function (err, res) {
+        if (err) throw err
+
+        console.table(res)
+        start()
+    });
+};
+
+// View list of employees by role
+function viewRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+
+        inquirer.prompt ([
+            {
+                type: "list",
+                name: "role",
+                message: "Which Role's Employees would you like to see?",
+                choices: function () {
+                    let choiceArray = []
+                    for (let i = 0; i < res.length; i++) {
+                        choiceArray.push(res[i].title)
+                    }
+                    return choiceArray
+                }
+            }
+        ]).then(function (answer) {
+            connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.department FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id", function (err, res) {
+                let roleArr = []
+                for (let i = 0; i < res.length; i++) {
+                    if (answer.role === res[i].title) {
+                        roleArr.push(res[i])
+                    }
+                }
+                console.table(roleArr);
+                start();
+            });
+        });
+    });
+};
+
+// View list of employees by department
